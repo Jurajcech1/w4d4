9d5f3b3787abff9efbb337980bf1ba5208a9bdbd
Jurajcech1 <Jurajcech1@gmail.com>
2015-11-19 11:56:28 -0800

    finished user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,16 @@ class User < ActiveRecord::Base
   attr_reader :password
   validates :email, :password_digest, :session_token, presence: true
   validates :email, uniqueness: true
-  validate :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user  = User.find_by(email: email)
+
+    return nil if user.nil?
+
+    user.is_password?(password) ? user : nil
+  end
 
   def is_password?(password)
     BCrypt::Password.new(password_digest).is_password?(password)
@@ -12,6 +20,7 @@ class User < ActiveRecord::Base
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
 
   def ensure_session_token
     self.session_token ||= SecureRandom::urlsafe_base64
